2e5cb622dd492296b44b660b4dc58032226fc6f6
Etimbuk Afia <ghostbandit02@gmail.com>
2024-07-30 19:54:58 +0100

    descriptions embedded

--- src/db_connect.py
+++ src/db_connect.py
@@ -2,6 +2,11 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import os
 from gridfs import GridFS
 from pymongo import MongoClient
+import logging
+
+#Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class Database:
     _client = None
@@ -15,14 +20,17 @@ class Database:
             password = os.environ.get('PASSWORD')
             username = os.environ.get('USERNAME')
             try:
-                uri = f"mongodb+srv://{username}:{password}@realestatecluster.kypuqxb.mongodb.net/?retryWrites=true&w=majority&appName=realEstateCluster"
+                uri = f"mongodb+srv://ghostbandit02:dontlogmein@realestatecluster.kypuqxb.mongodb.net/?retryWrites=true&w=majority&appName=realEstateCluster"
+                print(f"Connecting to MongoDB with URI: {uri}")
                 db_name = "listings"
                 collection_name = "austin_reduced"
                 cls._client = AsyncIOMotorClient(uri)
                 cls._db = cls._client[db_name]
                 cls._collection = cls._db[collection_name]
                 cls._fs = GridFS(MongoClient(uri)['listings'])
+                logger.info("Connected to MongoDB")
             except Exception as e:
+                logger.error(f"Failed to connect to MongoDB: {e}")
                 raise ConnectionError(f"Failed to connect to MongoDB: {e}")
         return cls._collection
     
@@ -30,4 +38,8 @@ class Database:
     async def close(cls):
         if cls._client:
             cls._client.close()
+            cls._client = None
+            cls._db = None
+            cls._collection = None
+            logger.info("Disconnected from MongoDB")
     
